37d75783fde70d9e3d6c3ae3dad70467a6712886
Ayc0 <benjamin.koltes@gmail.com>
2018-06-20 22:53:53 +0200

    Start display

--- src/components/FavoriteProjects/FavoriteProjects.js
+++ src/components/FavoriteProjects/FavoriteProjects.js
@@ -1,5 +1,28 @@
 import { h } from 'preact';
 
-const FavoriteProjects = () => <div>Favorite Projects 0.0.6</div>;
+import groupBy from 'lodash/groupBy';
+
+import projects from '%/utils/projects';
+
+const FavoriteProjects = () => {
+	const categories = groupBy(projects, 'language');
+	return (
+		<table>
+			{Object.entries(categories).map(([categoryName, categoryProjects]) => [
+				<thead key="head">
+					<th>{categoryName}</th>
+				</thead>,
+				<tbody key="body">
+					{categoryProjects.map(project => (
+						<tr key={project.name}>
+							<td>{project.name}</td>
+							<td>{project.description}</td>
+						</tr>
+					))}
+				</tbody>
+			])}
+		</table>
+	);
+};
 
 export default FavoriteProjects;
