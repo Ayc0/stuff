c94ed01c8858c263b04f182ef1f1244cc28260ef
Ayc0 <benjamin.koltes@gmail.com>
2018-06-21 10:32:09 +0200

    Alias by sed in package.json

--- preact.config.js
+++ preact.config.js
@@ -1,19 +1,13 @@
 const path = require('path');
 
-// ExtractTextPlugin
-
-const findPluginsByName = (config, name) => config.plugins.filter(plugin => plugin.constructor.name === name);
+// const findPluginsByName = (config, name) => config.plugins.filter(plugin => plugin.constructor.name === name);
 
 export default (config, env, helpers) => {
   // Find configs / plugins
   const babelConfig = helpers.getLoadersByName(config, 'babel-loader')[0].rule.options;
-  const extractTextPlugin = findPluginsByName(config, 'ExtractTextPlugin')[0];
 
   // Add Emotion in babelrc
   babelConfig.plugins.push('emotion');
-
-  // Say to CSS that url starts with /stuff/
-  extractTextPlugin.filename = 'stuff/' + extractTextPlugin.filename;
 
   // Set alias for $
   config.resolve.alias['%'] = path.resolve(__dirname, 'src');
