be7d97aa2cf6041e8ccf52b9e715ff99e7d7893b
Ayc0 <benjamin.koltes@gmail.com>
2018-06-21 09:19:10 +0200

    Search field

---
+++ src/components/Search/Search.js
@@ -0,0 +1,79 @@
+import { h, Component } from 'preact';
+import styled from 'preact-emotion';
+
+import getValue from '%/utils/getValue';
+import { getColor } from '%/utils/colors';
+
+import MagnifyingGlass from './MagnifyingGlass';
+
+const Wrapper = styled.div({
+  position: 'relative',
+  fontSize: '1.2em'
+});
+
+const MGWrapper = styled.div({
+  position: 'absolute',
+  left: '0.4em',
+  top: '0.15em'
+});
+
+const Input = styled.input({
+  backgroundColor: 'inherit',
+  borderRadius: '2em',
+  paddingLeft: '1.5em',
+  height: '100%',
+  border: 'none',
+  fontSize: '1em',
+  outline: 'none',
+  boxShadow: '0 0 0 1px black',
+  ':focus': {
+    boxShadow: `0 0 0 1px ${getColor('primary')}`,
+    color: getColor('primary-d1'),
+    '::placeholder': {
+      color: getColor('primary-d3')
+    }
+  }
+});
+
+class Search extends Component {
+  state = { focus: false };
+
+  blur = () => this.setState({ focus: false });
+  focus = () => this.setState({ focus: true });
+
+  ref = ref => {
+    this.input = ref;
+  };
+
+  setFocus = () => this.input.focus();
+
+  input = null;
+
+  render({ onChange, ...props }) {
+    return (
+      <Wrapper>
+        <MGWrapper onClick={this.setFocus}>
+          <MagnifyingGlass color={this.state.focus ? getColor('primary-d1') : 'black'} size="1.2em" />
+        </MGWrapper>
+        <Input
+          {...props}
+          innerRef={this.ref}
+          type="text"
+          onInput={getValue(onChange)}
+          onBlur={this.blur}
+          onFocus={this.focus}
+        />
+      </Wrapper>
+    );
+  }
+}
+
+Search.defaultProps = {
+  className: '',
+  value: '',
+  style: {},
+  placeholder: '',
+  onChange: () => {}
+};
+
+export default Search;
