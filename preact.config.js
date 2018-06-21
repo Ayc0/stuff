306b96df82d298a27da01cedb12635bb4e8d3619
Ayc0 <benjamin.koltes@gmail.com>
2018-06-21 10:16:46 +0200

    Change config

--- preact.config.js
+++ preact.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 
+// ExtractTextPlugin
+
+const findPluginsByName = (config, name) => config.plugins.filter(plugin => plugin.constructor.name === name);
+
 export default (config, env, helpers) => {
-	// Find Babel config
-	const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
-	const babelConfig = rule.options;
+  // Find configs / plugins
+  const babelConfig = helpers.getLoadersByName(config, 'babel-loader')[0].rule.options;
+  const extractTextPlugin = findPluginsByName(config, 'ExtractTextPlugin')[0];
 
-	// Add Emotion in babelrc
-	babelConfig.plugins.push('emotion');
+  // Add Emotion in babelrc
+  babelConfig.plugins.push('emotion');
 
-	// Set alias for $
-	config.resolve.alias['%'] = path.resolve(__dirname, 'src');
+  // Say to CSS that url starts with /stuff/
+  extractTextPlugin.filename = 'stuff/' + extractTextPlugin.filename;
+
+  // Set alias for $
+  config.resolve.alias['%'] = path.resolve(__dirname, 'src');
 };
