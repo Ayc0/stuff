7ef54a8e073ea2e80527f4f3f705b79c98e16d60
Ayc0 <benjamin.koltes@gmail.com>
2018-06-21 09:20:09 +0200

    Tag

---
+++ src/components/Tag/Tag.js
@@ -0,0 +1,48 @@
+import { h } from 'preact';
+import styled from 'preact-emotion';
+
+import getValue from '%/utils/getValue';
+import { getColor } from '%/utils/colors';
+
+const Wrapper = styled.div(
+  {
+    border: 'none',
+    borderRadius: '1em',
+    padding: '.25em .5em',
+    fontWeight: 600
+  },
+  ({ active, color }) => ({
+    backgroundColor: active ? getColor(color) : undefined,
+    boxShadow: active ? undefined : `0 0 0 2px ${getColor(color)}`,
+    color: active ? 'white' : getColor(color)
+  })
+);
+
+const Checkbox = styled.input({
+  display: 'none'
+});
+
+const Label = styled.label({
+  cursor: 'pointer'
+});
+
+let counter = 0;
+
+const Tag = ({ active, children, onToggle, color, ...props }) => {
+  const id = `tag__${counter++}`;
+  return (
+    <Wrapper active={active} color={color}>
+      <Checkbox id={id} type="checkbox" checked={active} onChange={getValue(onToggle)} />
+      <Label htmlFor={id}>{children}</Label>
+    </Wrapper>
+  );
+};
+
+Tag.defaultProps = {
+  color: 'primary',
+  active: false,
+  onToggle: () => {},
+  children: null
+};
+
+export default Tag;
