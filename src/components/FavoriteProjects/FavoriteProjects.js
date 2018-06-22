7c5e05761bda3015550c5695089484b81aba9a5b
Ayc0 <benjamin.koltes@gmail.com>
2018-06-22 11:54:07 +0200

    Deselect tag in unlisted

--- src/components/FavoriteProjects/FavoriteProjects.js
+++ src/components/FavoriteProjects/FavoriteProjects.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import styled from 'preact-emotion';
 import groupBy from 'lodash/groupBy';
 import flatten from 'lodash/flatten';
 import uniq from 'lodash/uniq';
@@ -8,21 +9,35 @@ import Row from '%/components/Row';
 import Tag from '%/components/Tag';
 import projects, { search } from '%/utils/projects';
 
+const TagList = styled(Row)({ padding: '.6em 0 .3em 0', marginBottom: '2em', maxHeight: '3.2em', overflow: 'auto' });
+
+const getKeywordsFromProjects = projects => uniq(flatten(projects.map(project => project.keywords))).sort();
+
 class FavoriteProjects extends Component {
-  state = { term: '' };
+  state = { term: '', selectedTags: [] };
   projects = projects;
+  keywords = getKeywordsFromProjects(projects);
 
   onChange = term => this.setState({ term });
+
+  onToggle = keyword =>
+    this.setState(({ selectedTags }) => {
+      if (!selectedTags.includes(keyword)) {
+        return { selectedTags: [...selectedTags, keyword] };
+      }
+      return { selectedTags: selectedTags.filter(tag => tag !== keyword) };
+    });
 
   componentWillUpdate(nextProps, nextState) {
     if (this.state.term !== nextState.term) {
       this.projects = search(nextState.term);
+      this.keywords = getKeywordsFromProjects(this.projects);
+      nextState.selectedTags = nextState.selectedTags.filter(tag => this.keywords.includes(tag));
     }
   }
 
-  render({}, { term }) {
-    const categories = groupBy(this.projects, 'language');
-    const keywords = uniq(flatten(this.projects.map(project => project.keywords))).sort();
+  render({}, { term, selectedTags }) {
+    const categories = groupBy(projects, 'language');
     return (
       <div>
         <Search
@@ -31,9 +46,13 @@ class FavoriteProjects extends Component {
           onChange={this.onChange}
           style={{ wrapper: { maxWidth: '15em' } }}
         />
-        <Row style={{ padding: '.6em 0 .3em 0', marginBottom: '2em', maxHeight: '3.2em', overflow: 'auto' }}>
-          {keywords.map(keyword => <Tag>{keyword}</Tag>)}
-        </Row>
+        <TagList>
+          {this.keywords.map(keyword => (
+            <Tag onToggle={this.onToggle} active={selectedTags.includes(keyword)}>
+              {keyword}
+            </Tag>
+          ))}
+        </TagList>
         <table>
           {Object.entries(categories).map(([categoryName, categoryProjects]) => [
             <thead key={`head_${categoryName}`}>
