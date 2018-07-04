275e9110cbfa97f36a1577e2ccbbf4097cc908d6
Ayc0 <benjamin.koltes@gmail.com>
2018-07-04 12:06:10 +0200

    Add extensions

--- src/components/Extensions/Extensions.js
+++ src/components/Extensions/Extensions.js
@@ -1,5 +1,33 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
-const Extensions = () => <div>Extensions</div>;
+import Search from '%/components/Search';
+import Row from '%/components/Row';
+
+import list, { search } from '%/utils/extensions';
+
+import Extension from './Extension';
+
+class Extensions extends Component {
+  state = { term: '' };
+
+  extensions = list;
+
+  onChange = term => this.setState({ term });
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.term !== this.state.term) {
+      this.extensions = search(nextState.term);
+    }
+  }
+
+  render({}, { term }) {
+    return (
+      <div>
+        <Search placeholder="Search extension" value={term} onChange={this.onChange} />
+        <Row column>{this.extensions.map(extension => <Extension extension={extension} />)}</Row>
+      </div>
+    );
+  }
+}
 
 export default Extensions;
