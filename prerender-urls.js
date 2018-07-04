e93f3563dc29445191aac6513cee248286ef811d
Ayc0 <benjamin.koltes@gmail.com>
2018-07-04 12:15:54 +0200

    0.2.2

--- prerender-urls.js
+++ prerender-urls.js
@@ -2,5 +2,13 @@ module.exports = [
   {
     url: '/',
     title: 'Ayc0 - Packages browser'
+  },
+  {
+    url: '/projects',
+    title: 'Ayc0 - Packages browser'
+  },
+  {
+    url: '/extensions',
+    title: 'VS Code extensions'
   }
 ];
