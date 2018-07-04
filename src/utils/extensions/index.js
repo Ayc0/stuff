3adfe3ea1944eb3464272e2b391adad38076e3bd
Ayc0 <benjamin.koltes@gmail.com>
2018-07-04 11:50:15 +0200

    Add extensions search

--- src/utils/extensions/index.js
+++ src/utils/extensions/index.js
@@ -1,10 +1,29 @@
+import Fuse from 'fuse.js';
+import sortBy from 'lodash/sortBy';
+
 import rawList from './list.json';
 
-const list = rawList.map(extension => {
-  if (extension.icon) {
-    extension.icon = require('./icons/' + extension.icon);
-  }
-  return extension;
+const list = sortBy(
+  rawList.map(extension => {
+    if (extension.icon) {
+      extension.icon = require('./icons/' + extension.icon);
+    }
+    return extension;
+  }),
+  'name'
+);
+
+const fuse = new Fuse(list, {
+  shouldSort: true,
+  threshold: 0.4,
+  keys: ['name', { name: 'description', value: 0.4 }]
 });
+
+export const search = term => {
+  if (!term) {
+    return list;
+  }
+  return fuse.search(term);
+};
 
 export default list;
